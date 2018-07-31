620242f35d7168c41557b939dd860b61e6918df0
Alexander Hicks <alex@Alexanders-MBP.satx.rr.com>
2018-07-30 21:19:39 -0500

    Moved docs location and improved code organization

--- nodeServer/log.js
+++ nodeServer/log.js
@@ -1,13 +1,28 @@
 var colors = require('colors');
-
+/** @module */
+/**
+ * A function for logging errors in red
+ * @param {string} tag - The log tag for easier searching
+ * @param {string} msg - The message to be logged
+ */
 exports.error = (tag,msg) => {
-  console.log(tag.bold+" "+msg.red);
+  console.log('['+tag.bold+'] '+msg.red);
 }
 
+/**
+ * A function for logging warnings in orange
+ * @param {string} tag - The log tag for easier searching
+ * @param {string} msg - The message to be logged
+ */
 exports.warn = (tag,msg) => {
-  console.log(tag.bold+" "+msg.orange);
+  console.log('['+tag.bold+'] '+msg.orange);
 }
 
+/**
+ * A function for logging messages in green
+ * @param {string} tag - The log tag for easier searching
+ * @param {string} msg - The message to be logged
+ */
 exports.info = (tag,msg) => {
-  console.log(tag.bold+" "+msg.green);
+  console.log('['+tag.bold+'] '+msg.green);
 }
